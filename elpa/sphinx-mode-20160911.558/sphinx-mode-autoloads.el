0843725307b5c9a3f2c4bbd71a8df514e77d2f1d
Sebastian Mika <sebastian.mika@comtravo.com>
2017-09-29 13:24:20 +0200

    current state

---
+++ elpa/sphinx-mode-20160911.558/sphinx-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; sphinx-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "sphinx-mode" "sphinx-mode.el" (22726 35041
+;;;;;;  0 0))
+;;; Generated autoloads from sphinx-mode.el
+
+(autoload 'sphinx-mode "sphinx-mode" "\
+Sphinx minor mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("sphinx-mode-pkg.el" "sphinx-src.el")
+;;;;;;  (22726 35041 107578 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sphinx-mode-autoloads.el ends here
